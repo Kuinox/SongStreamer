5d449ca05ae4539f7d0fb1ec550d1626b76a97dd
Kuinox <nvandeginste@gmail.com>
2018-09-06 01:09:19 +0200

    Project init

---
+++ WebApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using CK.Monitoring;
+namespace WebApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new WebHostBuilder()
+                .UseUrls("http://localhost:4324")
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseMonitoring()
+                .ConfigureAppConfiguration((hostBuilderContext, confBuilder) =>
+                {
+                    confBuilder
+                        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                        .AddJsonFile($"appsettings.{hostBuilderContext.HostingEnvironment.EnvironmentName}.json", optional: true, reloadOnChange: true)
+                        .AddEnvironmentVariables();
+
+                })
+                .UseKestrel()
+                .UseStartup<Startup>();
+
+            builder.Build().Run();
+        }
+    }
+}
---
+++ WebApp/Startup.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WebApp
+{
+    class Startup
+    {
+        readonly IConfiguration _configuration;
+        readonly IHostingEnvironment _env;
+
+        public Startup(IConfiguration configuration, IHostingEnvironment env)
+        {
+            _env = env;
+            _configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddOptions();
+            services.AddMvc();
+        }
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseRequestMonitor();
+            app.UseCors(c => c.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader().AllowCredentials());
+            app.UseAuthentication();
+            if (env.IsDevelopment()) app.UseDeveloperExceptionPage();
+
+            app.UseMvcWithDefaultRoute();
+            app.UseStaticFiles();   
+        }
+    }
+}
