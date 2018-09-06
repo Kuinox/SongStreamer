6989ed631671ce52526351468403fd781be8ac0f
Kuinox <nvandeginste@gmail.com>
2018-09-06 22:10:05 +0200

    Added IFileSystem

---
+++ StorageManagement/IFileSystem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace StorageManagement
+{
+    public interface IFileSystem
+    {
+        Task Store(Stream file, CancellationToken cancellationToken);
+        Task<Stream> Get(string fileLocation);
+        Task Remove(string fileLocation);
+    }
+}
